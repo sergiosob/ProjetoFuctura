e830d18832d427055e25c12043fab934eb339f4e
sergiosob <sobrito@gmail.com>
2021-05-24 10:50:33 -0300

    criando ambiente

---
+++ src/entities/Conta.java
@@ -0,0 +1,100 @@
+package entities;
+
+import java.util.Scanner;
+
+public class Conta {
+    private String nome;
+    private String tipo;
+    private int saques;
+    private double saldo;
+    Scanner entrada = new Scanner(System.in);
+    
+    
+    public Conta(String tipo, String nome, double saldo_inicial){
+        this.nome=nome;
+        this.tipo = tipo;
+        saldo=saldo_inicial;
+        saques=0;
+    }
+    
+    public void extrato(){
+        System.out.println("EXTRATO");
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Tipo da conta: " + this.tipo);
+        System.out.printf("Saldo atual: %.2f\n",this.saldo);
+        System.out.println("Saques realizados hoje: " + this.saques + "\n");
+        
+    }
+    
+    public void sacar(double valor){
+        if(saldo >= valor){
+            saldo -= valor;
+            saques++;
+            System.out.println("Sacado: " + valor);
+            System.out.println("Novo saldo: " + saldo + "\n");
+        } else {
+            System.out.println("Saldo insuficiente. Faça um depósito\n");
+        }
+    }
+    
+    public void depositar(double valor)
+    {
+        saldo += valor;
+        System.out.println("Depositado: " + valor);
+        System.out.println("Novo saldo: " + saldo + "\n");
+    }
+    
+    public void iniciar(){
+    
+        int opcao;
+
+        do{
+            exibeMenu();
+            opcao = entrada.nextInt();
+            escolheOpcao(opcao);
+        }while(opcao!=4);
+    }
+    
+    public void exibeMenu(){
+    	
+        System.out.println("Escolha a opção desejada");
+        System.out.println("1 - Consultar Extrato");
+        System.out.println("2 - Sacar");
+        System.out.println("3 - Depositar");
+        System.out.println("4 - Sair");
+        System.out.println("Opção: ");
+        
+    }
+    
+    public void escolheOpcao(int opcao){
+        double valor;
+        
+        switch( opcao ){
+            case 1:    
+                    extrato();
+                    break;
+            case 2: 
+                    if(saques<3){
+                        System.out.print("Quanto deseja sacar: ");
+                        valor = entrada.nextDouble();
+                        sacar(valor);
+                    } else{
+                        System.out.println("Limite de saques diários atingidos.\n");
+                    }
+                    break;
+                    
+            case 3:
+                    System.out.print("Quanto deseja depositar: ");
+                    valor = entrada.nextDouble();
+                    depositar(valor);
+                    break;
+                    
+            case 4: 
+                    System.out.println("Sistema encerrado.");
+                    break;
+                    
+            default:
+                    System.out.println("Opção inválida");
+        }
+    }
+}
